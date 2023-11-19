40818f13cbd0d37c0a072f9634c1f0d6d252120a
Subham <subhamsingh5638@gmail.com>
2023-11-19 20:57:43 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../App.css';
+
+const Header = () => {
+    // const logo = "https://lh3.googleusercontent.com/q1csmL5PsdGsbDP0ChfX4Fyw_YaXjNeWb68Zkjqy_NWsi9pqDyZUFWR4Th40Qm-143-EVFeSySa0=s0";
+
+    return (
+        <> 
+        {/* <img src = {logo} alt = "logo"  className="logo"/> */}
+        <h2>Text to Speech </h2>
+        </>
+    )
+}
+
+export default Header;

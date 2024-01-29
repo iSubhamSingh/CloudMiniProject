e8c7353fbdce586cf88ad176777fda6143847975
Subham <subhamsingh5638@gmail.com>
2024-01-29 17:16:12 +0530

    added logo in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import '../App.css';
 
 const Header = () => {
-    // const logo = "https://lh3.googleusercontent.com/q1csmL5PsdGsbDP0ChfX4Fyw_YaXjNeWb68Zkjqy_NWsi9pqDyZUFWR4Th40Qm-143-EVFeSySa0=s0";
+    const logo = "https://imgs.search.brave.com/ByznXcI9NNe1fDA7hCmAauY2bzFkaZxO46t3s3uqGTU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pdGNo/cm9uaWNsZXMuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIw/LzEwL3NwZWVjaC10/by10ZXh0LTEwMjR4/NjgzLmpwZw";
 
     return (
         <> 
-        {/* <img src = {logo} alt = "logo"  className="logo"/> */}
+        <img src = {logo} alt = "logo"  className="logo"/>
         <h2>Text to Speech </h2>
         </>
     )

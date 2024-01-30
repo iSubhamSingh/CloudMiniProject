58144b1e394cbac8c92afcccd2915a1824808012
Subham Singh <subhamsingh5638@gmail.com>
2024-01-30 19:28:40 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import '../App.css';
 
 const Header = () => {
-    const logo = "https://imgs.search.brave.com/ByznXcI9NNe1fDA7hCmAauY2bzFkaZxO46t3s3uqGTU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pdGNo/cm9uaWNsZXMuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIw/LzEwL3NwZWVjaC10/by10ZXh0LTEwMjR4/NjgzLmpwZw";
+    const logo = "https://shorturl.at/lmrP3";
 
     return (
         <> 
